0ebb7d27ee6378fcfa5d10642f1cc24f44ab0818
neuronhospital <neuronhospitallatur@gmail.com>
2026-09-07 18:42:22 +0530

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,5 +1,5 @@
 window.$=window.U?.$||((id)=>document.getElementById(id));
-if("serviceWorker"in navigator)window.addEventListener("load",()=>navigator.serviceWorker.register("./service-worker.js").catch(()=>{}));
+if("serviceWorker"in navigator)window.addEventListener("load",()=>{const v=encodeURIComponent(window.NEURON_CONFIG.appVersion);navigator.serviceWorker.addEventListener("controllerchange",()=>{if(!sessionStorage.getItem("neuron-sw-reloaded-"+v)){sessionStorage.setItem("neuron-sw-reloaded-"+v,"1");location.reload();}});navigator.serviceWorker.register("./service-worker.js?v="+v).catch(()=>{});});
 document.addEventListener("DOMContentLoaded",()=>{
  const p=document.body.dataset.title||"Portal",h=document.getElementById("header"),f=document.getElementById("footer");
  if(h)h.innerHTML=`<header class="site-header"><div class="container header-inner"><a class="brand" href="index.html"><img class="brand-logo" src="assets/neuron_logo.png" alt="NEURON Hospital Logo"><div><div class="brand-title">NEURON Hospital</div><div class="brand-sub">Latur • Pediatric Neurology & Epilepsy Center</div></div></a><a class="header-home" href="index.html"><img src="assets/icons/home.svg" alt="Home"></a></div></header>`;
